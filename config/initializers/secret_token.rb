6f7babfe074530f205cb107aac43dd3bbe442189
ylobo <yule234@gmail.com>
2013-09-29 21:50:05 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppStoreReviews::Application.config.secret_token = 'd3c0b110eee1cfff733c35eb53e54b7287e3695fecaebb2c3c94c4a1cd45c5e0b14d5ad2a33aa07fffa9bd439b834b90589a4d0c1753bb0f52147cf14c25b069'
