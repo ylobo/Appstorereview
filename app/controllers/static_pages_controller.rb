d689342c12fba6ee83ddc85d4b6a5586e2ade1ad
ylobo <yule234@gmail.com>
2014-03-09 19:37:35 +0530

    Added Error Handling on Search page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,23 +13,28 @@ class StaticPagesController < ApplicationController
   def results
     #flash.now[:success]=params[:options]
      params[:query1] = params[:query]||params[:query1]
-     #params[:options] = params[:options]
-     case params[:options][0]
-     when "Apple Store"   
-      report = SummaryReporterHelper::Summary.new(SummaryReporterHelper::AppleStoreSummary.new,params[:query]||params[:query1])
-      report.getSummary
-      @userRatingCountForCurrentVersion=report.userRatingCountForCurrentVersion
-      @averageUserRatingForCurrentVersion=report.averageUserRatingForCurrentVersion
-      @averageUserRating=report.averageUserRating
-      @userRatingCount=report.userRatingCount
-      @artworkUrl60=report.artWorkUrl
-      @artistName=report.artistName
-      @artWorkUrl=report.artWorkUrl
-     when "Google Store" 
-      report = SummaryReporterHelper::Summary.new(SummaryReporterHelper::GoogleStoreSummary.new,params[:query]||params[:query1])
-      report.getSummary
-     else
+     params[:options] = params[:options]
+     if ((params[:query1]||params[:query2])!="")
+      case params[:options][0]
+      when "Apple Store"   
+        report = SummaryReporterHelper::Summary.new(SummaryReporterHelper::AppleStoreSummary.new,params[:query]||params[:query1])
+        report.getSummary
+        @userRatingCountForCurrentVersion=report.userRatingCountForCurrentVersion
+        @averageUserRatingForCurrentVersion=report.averageUserRatingForCurrentVersion
+        @averageUserRating=report.averageUserRating
+        @userRatingCount=report.userRatingCount
+        @artworkUrl60=report.artWorkUrl
+        @artistName=report.artistName
+        @artWorkUrl=report.artWorkUrl
+      when "Google Store" 
+        report = SummaryReporterHelper::Summary.new(SummaryReporterHelper::GoogleStoreSummary.new,params[:query]||params[:query1])
+        report.getSummary
+      else
        flash.now[:success]=params[:options]
+      end  
+     else
+       redirect_to root_path
+       flash.now[:error] = "Query field cannot be left empty"
      end  
   end
 end
